8071e11a19fcb4de988186cf6c0665bf6b6c1f2d
eshun4 <eshunkofi23@gmail.com>
2022-01-24 22:03:28 -0700

    Bottom Information Added

--- E-Portfolio/js/carousel.js
+++ E-Portfolio/js/carousel.js
@@ -1,15 +1,15 @@
-var slideIndex = 0;
-showSlides();
+var slideIndex = 1;
+showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
-  
+  showSlides(slideIndex += n);
+}
+
 // Thumbnail image controls
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
-  }
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
 function showSlides() {
   var i;
