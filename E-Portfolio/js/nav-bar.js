0b02dcdc38d7ec5f6aee7661610d148efc1e3714
eshun4 <eshunkofi23@gmail.com>
2022-03-25 02:26:11 -0600

    Last commit for thursday

--- E-Portfolio/js/nav-bar.js
+++ E-Portfolio/js/nav-bar.js
@@ -1,16 +1,29 @@
 var toggleButton = document.querySelector('.toggle-button');
 var mobileNav = document.querySelector('.mobile-nav');
 var backdrop = document.querySelector(".backdrop");
+var selectPlanButton = document.querySelector(".banner_contactMe__icon");
+var modal = document.querySelector('.modal');
+var closeButton = document.querySelector(".fa-close");
 
+
+selectPlanButton.addEventListener('click', function(){
+    modal.style.display ="block";
+    backdrop.style.display ="block";
+})
 
 function closeModal() {
     backdrop.style.display = "none";
 }
 
-backdrop.addEventListener("click", function() {
+closeButton.addEventListener('click', function(){
+    modal.style.display ="none";
+    backdrop.style.display = "none";
     mobileNav.style.display = 'none';
-    closeModal();
-});
+})
+
+// backdrop.addEventListener("click", function() {
+    
+// });
 
 toggleButton.addEventListener('click', function() {
     mobileNav.style.display = 'block';
